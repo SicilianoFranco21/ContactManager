c78ef8bb0ac020c650b2e0d9c45b3709e14f0fe8
SicilianoFranco21 <sicilianofranco21@gmail.com>
2025-04-28 16:53:14 -0300

    Modify run_tests and add new imports for typing purposes

--- run_tests.py
+++ run_tests.py
@@ -1,38 +1,7 @@
-from unittest import TestCase, TestSuite, TextTestResult, TextTestRunner, TestLoader
-import time
-
-class PrettyTestResult(TextTestResult):
-    def __init__(self, *args, **kwargs) -> None:
-        super().__init__(*args, **kwargs)
-        self.successes: list[TestCase] = []
-
-    def addSuccess(self, test: TestCase) -> None:
-        super().addSuccess(test)
-        self.successes.append(test)
-        self.stream.writeln(f"✅ {test._testMethodName} - {test.shortDescription()}")
-
-    def addFailure(self, test: TestCase, err) -> None:
-        super().addFailure(test, err)
-        self.stream.writeln(f"❌ {test._testMethodName} - {test.shortDescription()}")
-
-    def addError(self, test: TestCase, err) -> None:
-        super().addError(test, err)
-        self.stream.writeln(f"⚠️ {test._testMethodName} - {test.shortDescription()}")
-
-    def print_summary(self, start_time: float, end_time: float) -> None:
-        total: int = self.testsRun
-        failed: int = len(self.failures)
-        errors: int = len(self.errors)
-        passed: int = len(self.successes)
-        duration: float = end_time - start_time
-
-        self.stream.writeln("\n📊 SUMMARY")
-        self.stream.writeln("───────────────")
-        self.stream.writeln(f"✅ Passed : {passed}")
-        self.stream.writeln(f"❌ Failed : {failed}")
-        self.stream.writeln(f"⚠️  Errors : {errors}")
-        self.stream.writeln(f"📦 Total  : {total}")
-        self.stream.writeln(f"⏱️ Duration: {duration:.4f} seconds")
+from time import time
+from tests.utils.prettySummaryFormatter import PrettySummaryFormatter
+from tests.utils.prettyTestResult import PrettyTestResult
+from unittest import TestLoader, TestSuite, TextTestRunner
 
 
 if __name__ == "__main__":
@@ -44,8 +13,9 @@ if __name__ == "__main__":
         resultclass=PrettyTestResult
     )
 
-    start: float = time.time()
+    start: float = time()
     result: PrettyTestResult = runner.run(suite)
-    end: float = time.time()
+    end: float = time()
 
-    result.print_summary(start, end)
+    formatter = PrettySummaryFormatter(result.stream)
+    formatter.format(result, start, end)
